07571d0967d8ae33e58fa0a33ed6c3c9ec304eae
RuanPablo-7 <ToyaMON777@outlook.com>
2025-08-14 17:30:03 -0300

    created my server to my projects

--- server.js
+++ server.js
@@ -2,9 +2,12 @@ import express from 'express';
 const app = express()
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static('public/my-web-site'))
-app.use('/jogodavelha', express.static('public/jogoDaVelha/jogodavelha'))
+// Serve my-web-site na raiz
+app.use(express.static('public/my-web-site'));
+
+// Serve jogodavelha na rota /jogodavelha
+app.use('/jogodavelha', express.static('public/jogoDaVelha/jogodavelha'));
 
 app.listen(port, () => {
-    console.log('working...')
-})
+  console.log(`Servidor rodando na porta ${port}`);
+});
