99d51301e5ec8730f4e71ad223fbe2a964376c4e
RuanPablo-7 <ToyaMON777@outlook.com>
2025-08-14 17:43:52 -0300

    created my server to my projects

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express()
 const port = process.env.PORT || 3000;
 
 // Serve my-web-site na raiz
-app.use(express.static('public/my-web-site'));
+app.use('/', express.static('public/my-web-site'));
 
 // Serve jogodavelha na rota /jogodavelha
 app.use('/jogodavelha', express.static('public/jogoDaVelha/jogodavelha'));
