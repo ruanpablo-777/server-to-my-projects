27084687ea19194731da4b1f134db9e01c0a5a2b
RuanPablo-7 <ToyaMON777@outlook.com>
2025-08-14 17:18:55 -0300

    created my server to my projects

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000;
 
 app.use('/', express.static('public/my-web-site'))
 app.use('/jogodavelha', express.static('public/jogoDaVelha/jogodavelha'))
