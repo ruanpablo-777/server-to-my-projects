2ee4224775b2156aa813e08c0f78ce58c2cccbca
RuanPablo-7 <ToyaMON777@outlook.com>
2025-08-14 16:56:01 -0300

    created my server to my projects

---
+++ server.js
@@ -0,0 +1,10 @@
+import express from 'express';
+const app = express()
+const port = 3000
+
+app.use('/', express.static('public/my-web-site'))
+app.use('/jogodavelha', express.static('public/jogoDaVelha/jogodavelha'))
+
+app.listen(port, () => {
+    console.log('working...')
+})
